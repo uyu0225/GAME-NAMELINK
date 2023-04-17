d22e9e683125d95df1fe33f26024b39c0638a924
後藤 優 <yu@gotounoMacBook-Air.local>
2023-04-17 23:27:36 +0900

    ログインページ修正

--- src/components/Login.js
+++ src/components/Login.js
@@ -22,12 +22,6 @@ const Login = () => {
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <h1 className="text-3xl">GAME-NAMELINK</h1>
-      <h2 className="my-3">【使い方】</h2>
-      <img className="w-3/5" src="/images/app-img.png" alt="" />
-      <p>1.googleアカウントでログインする</p>
-      <p>2.各入力欄に情報を入力する</p>
-      <p>3.フレンド追加ボタンを押す</p>
-      <p>4.フレンド一覧に新たなフレンドが追加される</p>
       {error && <p>{error}</p>}
       <button
         className="bg-blue-500 hover:bg-blue-700 cursor-pointer text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline block mt-3"
